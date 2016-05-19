5f05111892cf0750ac4c10f9b2292f87ae855372
Ben Anderson <BenjaminJaredAnderson@gmail.com>
2016-05-19 11:10:22 -0600

    After presentation

---
+++ HcUnitTest.Tests/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssertDemo.Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class CalculatorTest
+	{
+		public class AddInts
+		{
+			//static public object[][] AdditionTestCases = new[] { new object[] { 1, 1, 2, new Enemy() } };
+
+			[Test]
+			//[TestCaseSource(nameof(AdditionTestCases))]
+			[TestCase(4, 5, 9)]
+			[TestCase(0, 0, 0, TestName = "Adds Zeroes Correctly")]
+			[TestCase(-1, -1, -2)]
+			public void AddsCorrectly(int num1, int num2, int expectedResult, Enemy enemy)
+			{
+				// arrange
+				var calculator = new Calculator();
+
+				// act
+				var result = calculator.AddInts(num1, num2);
+
+				// assert
+				Assert.AreEqual(expectedResult, result, "did not expect addtion result");
+			}
+		}
+
+	}
+}
--- HcUnitTest/Calculator.cs
+++ HcUnitTest/Calculator.cs
@@ -2,28 +2,35 @@
 
 namespace AssertDemo
 {
-    public class Calculator
-    {
-        public int AddInts(int a, int b)
-        {
-            return a + b;
-        }
+	public class Calculator
+	{
+//		private readonly ILogger logger;
+//
+//		public Calculator(ILogger logger)
+//		{
+//			this.logger = logger;
+//		}
+
+		public int AddInts(int a, int b)
+		{
+			return a + b;
+		}
 
 
-        public double AddDoubles(double a, double b)
-        {
-            return a + b;
-        }
+		public double AddDoubles(double a, double b)
+		{
+			return a + b;
+		}
 
 
-        public int Divide(int value, int by)
-        {
-            if (value > 100)
-            {
-                throw new ArgumentOutOfRangeException("by"); // bug for demo purposes
-            }
+		public int Divide(int value, int by)
+		{
+			if (value > 100)
+			{
+				throw new ArgumentOutOfRangeException("by"); // bug for demo purposes
+			}
 
-            return value / by;
-        }
-    }
+			return value / by;
+		}
+	}
 }
